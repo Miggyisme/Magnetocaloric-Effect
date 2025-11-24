5c044e9a0fd373be41018c9febe2d0fddddc7572
Miguel Abranches <nickkelido@gmail.com>
2025-11-24 09:01:19 -0300

    Problema físico consertado

--- Ferromags/FerroMagT.py
+++ Ferromags/FerroMagT.py
@@ -60,12 +60,14 @@ F_results=[]
 
 
 def M(T,B):
+    # Adição
     m = 1
     tol=1e-8
     if B>0:
         m=1
     elif B<0:
         m=-1
+    # Adição
     while True:
         Bef_ = Bef(m,B)
         arg = (g*mb*Bef_) / (2*k*T)
